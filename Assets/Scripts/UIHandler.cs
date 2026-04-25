c156e0aca9bbea0b01970ead608eafc1f62f22bc
Kroacthu <wuazhihong@gmail.com>
2026-04-25 12:53:29 -0700

    Created a UI Scene
    UI Scene has a canvas of buttons and a UI handler
    The UI Handler detects the player using the esc key and can activate/deactivate the buttons on the UI.
    
    Also a main scene was put into the scenes folder

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+public class UIHandler : MonoBehaviour
+{
+    public Button escape;
+    public Button restart;
+    bool EscapeMenuOpen;
+    public InputAction ESCKey;
+    public float TimerSeconds = 0;
+    float Timer = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ESCKey.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timer > 0) { Timer -= Time.deltaTime; }
+        // When the esc key is pressed, the escape menu ui will be shown or hidden
+        if (ESCKey.IsPressed() && Timer <= 0)
+        {
+            Timer = TimerSeconds;
+            //Debug.Log("Escape key pressed");
+            if(!EscapeMenuOpen)
+            { 
+                escape.gameObject.SetActive(true); 
+                restart.gameObject.SetActive(true);
+                EscapeMenuOpen = true;
+            }
+            else
+            {
+                escape.gameObject.SetActive(false);
+                restart.gameObject.SetActive(false);
+                EscapeMenuOpen= false;
+            }
+        }
+    }
+
+    public void Escape()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(Application.dataPath);
+    }
+}
