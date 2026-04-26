ae1d848c65728291f1e71c66e0bca6bd4ab93382
mattmaomao <matthewcp220@gmail.com>
2026-04-26 10:32:24 -0700

    make rotate cards

--- Assets/Scripts/Hand Manager.cs
+++ Assets/Scripts/Hand Manager.cs
@@ -157,4 +157,12 @@ public class HandManager : TileCollection
         SelectCard(card);
     }
 
+    // debug -------------
+    [SerializeField] Tile debugCard;
+    [ContextMenu("Add A Card")]
+    void AddRandomCard()
+    {
+        Add(debugCard);
+    }
+
 }
